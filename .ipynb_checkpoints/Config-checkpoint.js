cfc029873f6f90addb4a3856d9d65f496315e68d
Caitlyn Myland <caitlyn.myland@gmail.com>
2024-02-10 17:35:43 -0500

    First Commit

---
+++ .ipynb_checkpoints/Config-checkpoint.js
@@ -0,0 +1,5 @@
+// config.js
+// var config = {
+// API_KEY: "pk.eyJ1Ijoic2NvdGhpcyIsImEiOiJjaWp1Y2ltYmUwMDBicmJrdDQ4ZDBkaGN4In0.sbihZCZJ56-fsFNKHXF8YQ"}
+// ;
+ export let config = "pk.eyJ1Ijoic2NvdGhpcyIsImEiOiJjaWp1Y2ltYmUwMDBicmJrdDQ4ZDBkaGN4In0.sbihZCZJ56-fsFNKHXF8YQ";
