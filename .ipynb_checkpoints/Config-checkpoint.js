81fd73fc2bb383a764582d2ae475440afffdccbd
Caitlyn Myland <caitlyn.myland@gmail.com>
2024-02-10 17:46:09 -0500

    Remove unecessary files

--- .ipynb_checkpoints/Config-checkpoint.js
+++
@@ -1,5 +0,0 @@
-// config.js
-// var config = {
-// API_KEY: "pk.eyJ1Ijoic2NvdGhpcyIsImEiOiJjaWp1Y2ltYmUwMDBicmJrdDQ4ZDBkaGN4In0.sbihZCZJ56-fsFNKHXF8YQ"}
-// ;
- export let config = "pk.eyJ1Ijoic2NvdGhpcyIsImEiOiJjaWp1Y2ltYmUwMDBicmJrdDQ4ZDBkaGN4In0.sbihZCZJ56-fsFNKHXF8YQ";
